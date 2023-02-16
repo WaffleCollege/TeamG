5aa6a8be558a93e02495f072e9f0b0bf795ca999
Satomi.Suzuki <stms.tms.358@gmail.com>
2023-02-17 01:36:56 +0900

    イベント情報表示画面のエンドポイントを作成しました

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 // Express Server インスタンスを作成
 let express = require("express");
 let app = express();
+
 //html,cssファイルを読み込む
 //イベント作成画面
 app.use("/create/event", express.static("event"));
-app.use("/",(req,res) => {
+app.use("/create/event",(req,res) => {
 })
 
-//イベント
+//イベント情報表示画面
+app.use("/event/information", express.static("イベント情報用のhtml,cssが入ったファイル名"));
+app.use("/event/information", (req,res) => {
+})
 
 //port番号
 app.listen(3000, () => {
