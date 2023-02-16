72bb9e893f0d45bd7fe1d7e7ba3506991ca0ea91
Satomi.Suzuki <stms.tms.358@gmail.com>
2023-02-17 01:33:16 +0900

    イベント作成画面のエンドポイントを変更しました

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 let express = require("express");
 let app = express();
 //html,cssファイルを読み込む
-app.use("/", express.static("event"));
+//イベント作成画面
+app.use("/create/event", express.static("event"));
 app.use("/",(req,res) => {
 })
+
+//イベント
 
 //port番号
 app.listen(3000, () => {
