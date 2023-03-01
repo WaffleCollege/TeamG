488889020c1311248b818cfca44228a50f2a9da4
Ayuna Hirashima <ayuna.kitoh@gmail.com>
2023-03-01 13:26:50 +0900

    今までの分です。データベースはnodejsで作ってます

--- test.js
+++ test.js
@@ -1,4 +1,34 @@
-console.log("hello");
-console.log("satoimo");
+var http = require('http');
+var html = require('fs').readFileSync('index.html');
+ 
+http.createServer(function(req, res) {
+ 
+  if(req.method === 'GET') {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.end(html);
+  }
+ 
+}).listen(3000);
 
-console.log("Yay");
+http.createServer(function(req, res) {
+    if(req.method === 'GET') {
+   
+      res.writeHead(200, {'Content-Type' : 'text/html'});
+      res.end(html);
+   
+    } else if(req.method === 'POST') {
+        var data = '';
+    
+   //POSTデータを受けとる
+   req.on('data', function(chunk) {data += chunk})
+      .on('end', function() {
+ 
+        console.log(data);
+        res.end(html);
+ 
+      })
+   
+          //ここにPOST受信処理を記述する
+   
+    }
+  }).listen(3000);
