95ed6b0effb62804a560cff6961565433135147b
Satomi.Suzuki <stms.tms.358@gmail.com>
2023-03-07 02:27:33 +0900

    app.jsと競合していたのでコメントアウトしてサーバが立つようにしました

--- test.js
+++ test.js
@@ -1,34 +1,34 @@
-var http = require('http');
-var html = require('fs').readFileSync('index.html');
+// var http = require('http');
+// var html = require('fs').readFileSync('index.html');
  
-http.createServer(function(req, res) {
+// http.createServer(function(req, res) {
  
-  if(req.method === 'GET') {
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.end(html);
-  }
+//   if(req.method === 'GET') {
+//     res.writeHead(200, {'Content-Type' : 'text/html'});
+//     res.end(html);
+//   }
  
-}).listen(3000);
+// }).listen(3000);
 
-http.createServer(function(req, res) {
-    if(req.method === 'GET') {
+// http.createServer(function(req, res) {
+//     if(req.method === 'GET') {
    
-      res.writeHead(200, {'Content-Type' : 'text/html'});
-      res.end(html);
+//       res.writeHead(200, {'Content-Type' : 'text/html'});
+//       res.end(html);
    
-    } else if(req.method === 'POST') {
-        var data = '';
+//     } else if(req.method === 'POST') {
+//         var data = '';
     
-   //POSTデータを受けとる
-   req.on('data', function(chunk) {data += chunk})
-      .on('end', function() {
+//    //POSTデータを受けとる
+//    req.on('data', function(chunk) {data += chunk})
+//       .on('end', function() {
  
-        console.log(data);
-        res.end(html);
+//         console.log(data);
+//         res.end(html);
  
-      })
+//       })
    
-          //ここにPOST受信処理を記述する
+//           //ここにPOST受信処理を記述する
    
-    }
-  }).listen(3000);
+//     }
+//   }).listen(3000);
